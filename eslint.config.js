a06d2b0f0bc4575e9a064cf9a5f69082cfc8dde5
MianyangYu <goutou7474@gmail.com>
2026-05-22 14:08:46 +0800

    chore: scaffold backend project

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.node,
+    },
+  },
+)
